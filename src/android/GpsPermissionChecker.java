ec4644b955345d8908e2072191071aebb5afdfb3
Andreas Halemba <andreas@hotelbird.de>
2016-12-20 14:11:48 +0100

    added native check for permission

--- src/android/GpsPermissionChecker.java
+++ src/android/GpsPermissionChecker.java
@@ -1,11 +1,13 @@
-package com.hotelbird.gpsPermissionChecker;
+package com.hotelbird.gpsPermissionChecker.GpsPermissionChecker;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -15,15 +17,20 @@ public class GpsPermissionChecker extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("checkGps")) {
-            this.coolMethod(callbackContext);
+            this.checkGpsPermission(callbackContext);
             return true;
         }
         return false;
     }
 
     private void checkGpsPermission(CallbackContext callbackContext) {
-
-            callbackContext.success("Permission granted.");
-            callbackContext.error("Expected one non-empty string argument.");
+        String gpsPermissionName = "android.permission.ACCESS_FINE_LOCATION";
+        Context context = this.cordova.getActivity().getApplicationContext();
+        int res = context.checkCallingOrSelfPermission(gpsPermissionName);
+        if (res == PackageManager.PERMISSION_GRANTED){
+            callbackContext.success("permission GPS given!!!");
+        } else {
+            callbackContext.error("permisson GPS not given!!!");
+        }
     }
 }
